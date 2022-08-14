ff63ef3b5fd10b1f3866aa053b1ce5ece0bf2b11
HuoHuas001 <2351078777@qq.com>
2022-08-14 09:56:59 +0800

    update

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,7 @@ import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
-
-    core.setOutput('time', new Date().toTimeString())
+    core.info(`Hello World`)
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
